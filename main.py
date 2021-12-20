8f8c6e352e0ab2ca1bcd47215afcd86ac3d81a5c
Andrew Boyer <asboyer@gmail.com>
2021-12-19 21:22:33 -0500

    changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import tweepy
 import secret
+import os
 
 auth = tweepy.OAuthHandler(secret.consumer_key, secret.consumer_secret)
 auth.set_access_token(secret.access_token, secret.access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print("______________")
+for tweet in tweepy.Cursor(api.user_timeline).items():
+    print(os.get_terminal_size().columns * "*")
     print(tweet.text)
