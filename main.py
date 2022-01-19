70a0daffa217b26a842ae370f4ea21fe9917f082
Andrew Boyer <asboyer@gmail.com>
2022-01-19 18:22:48 -0500

    new updates

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tweepy
 import secret
 import os
 from tweepy import OAuthHandler
+import requests
+import json
 
 auth = OAuthHandler(secret.consumer_key, secret.consumer_secret)
 auth.set_access_token(secret.access_token, secret.access_token_secret)
@@ -9,5 +11,35 @@ auth.set_access_token(secret.access_token, secret.access_token_secret)
 api = tweepy.API(auth)
 api.verify_credentials()
 
-for tweet in tweepy.Cursor(api.search_tweets, q='asboyer.com').items():
-   print(tweet.user.name)
+BEARER_TOKEN = secret.bearer_token
+#its bad practice to place your bearer token directly into the script (this is just done for illustration purposes)
+#define search twitter function
+def search_twitter(query, tweet_fields, bearer_token = BEARER_TOKEN):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
+        query, tweet_fields
+    )
+    response = requests.request("GET", url, headers=headers)
+
+    # print(response.status_code)
+
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+#search term
+query = "asboyer.com"
+#twitter fields to be returned by api call
+tweet_fields = "tweet.fields=text,author_id,created_at"
+
+#twitter api call
+json_response = search_twitter(query=query, tweet_fields=tweet_fields, bearer_token=BEARER_TOKEN)
+#pretty printing
+tweets = json_response['data']
+for t in tweets:
+    print(t)
+    username = api.get_user(user_id=t['author_id']).screen_name
+    api.update_status(status=f'@{username} congrats! you found the best website of all time!', in_reply_to_status_id=t['id'], auto_populate_reply_metadata=True)
+
+
