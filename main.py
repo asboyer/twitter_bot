cf7dbb8c7e2831c78713af7ae98b56603cf83c9a
boyer <asboyer@gmail.com>
2022-01-27 07:38:50 -0500

    quick fix w/ asboyer.com updates

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import twitter_lib
 
 while True:
     twitter_lib.reply_bot()
-    twitter_lib.new_blog_post()
+    try:
+        twitter_lib.new_blog_post()
+    except:
+        print('asboyer.com updated')
     twitter_lib.git_asboyer()
     time.sleep(2)
